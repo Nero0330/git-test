03d719c9f416528b350752af3e927ca01016b316
Nero <2814573813@qq.com>
2022-03-01 11:07:18 +0800

    push first commit

--- src/main/java/com/hzx/git/GitTest.java
+++ src/main/java/com/hzx/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello.txt test");
         System.out.println("xxds");
         System.out.println("yydi");
+        System.out.println("yyip");
     }
 }
