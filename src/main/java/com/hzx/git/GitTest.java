0ce3d8315ded96e72b3d0fab3aa04656eede789d
Nero <2814573813@qq.com>
2022-03-01 10:21:28 +0800

    first commit

---
+++ src/main/java/com/hzx/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.hzx.git;
+
+/**
+ * @description:
+ * @author:Nero
+ * @create:2022-03-01-10:17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hzxyyds");
+    }
+}
